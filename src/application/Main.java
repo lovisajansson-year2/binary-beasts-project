c7aff5723abb9972114d9dce6d7588d839fc2462
simonjobs <44189435+simonjobs@users.noreply.github.com>
2019-09-20 11:24:18 +0200

    frame

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("");
+        Parent root = FXMLLoader.load(getClass().getResource("../views/sample.fxml"));
+        primaryStage.setTitle("Binary Beasts");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
