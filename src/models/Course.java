88f4ae5d3a09a8ea7aef1e1041476a1b59ae2db5
SDorka <s.dorka582gmail.com>
2019-09-23 13:42:16 +0200

    java classes
    get and set methods

--- src/models/Course.java
+++ src/models/Course.java
@@ -1,4 +1,27 @@
 package models;
 
 public class Course {
+	private String courseCode;
+	private String courseName;
+	private int credits;
+	
+	public String getCourseCode() {
+		return courseCode;
+	}
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public int getCredits() {
+		return credits;
+	}
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+
 }
--- src/models/Student.java
+++ src/models/Student.java
@@ -1,4 +1,26 @@
 package models;
 
 public class Student {
+	private String lastName;
+	private String firstName;
+	private String studentID;
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(String studentID) {
+		this.studentID = studentID;
+	}
 }
