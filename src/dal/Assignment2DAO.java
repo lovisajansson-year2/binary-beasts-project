b78c34aa51491093a6d6a2f4264f9c6aa04feec1
le7352be <le7352be-s@student.lu.se>
2019-10-04 09:12:41 +0200

    ändrat SQL kod till lower case

--- src/dal/Assignment2DAO.java
+++ src/dal/Assignment2DAO.java
@@ -7,89 +7,89 @@ public class Assignment2DAO {
 	 public static String getStmt(int index) {
 		 String stmt = "";
 		 switch (index) {
-			 case 0: stmt = "SELECT No_, [First Name], [Last Name], Address, City\n" +
-					 "FROM [CRONUS Sverige AB$Employee]\n";
+			 case 0: stmt = "select No_, [First Name], [Last Name], Address, City\n" +
+					 "from [CRONUS Sverige AB$Employee]\n";
 			 break;
-			 case 1: stmt = "SELECT [Employee No_], [From Date], [To Date], Description, Quantity\n" +
-					 "FROM [CRONUS Sverige AB$Employee Absence]\n";
+			 case 1: stmt = "select [Employee No_], [From Date], [To Date], Description, Quantity\n" +
+					 "from [CRONUS Sverige AB$Employee Absence]\n";
 			 break;
-			 case 2: stmt = "SELECT timestamp, [Search Limit], [Temp_ Key Index], [Temp_ Table No_], [Temp_ Option Value]\n" +
-					 "FROM [CRONUS Sverige AB$Employee Portal Setup]\n";
+			 case 2: stmt = "select timestamp, [Search Limit], [Temp_ Key Index], [Temp_ Table No_], [Temp_ Option Value]\n" +
+					 "from [CRONUS Sverige AB$Employee Portal Setup]\n";
 			 break;
-			 case 3: stmt = "SELECT [Employee No_], [Qualification Code], Description, Institution_Company, Cost\n" +
-					 "FROM [CRONUS Sverige AB$Employee Qualification]\n";
+			 case 3: stmt = "select [Employee No_], [Qualification Code], Description, Institution_Company, Cost\n" +
+					 "from [CRONUS Sverige AB$Employee Qualification]\n";
 			 break;
-			 case 4: stmt = "SELECT [Employee No_], [Relative Code], [First Name], [Last Name], [Birth Date]\n" +
-					 "FROM [CRONUS Sverige AB$Employee Relative]\n";
+			 case 4: stmt = "select [Employee No_], [Relative Code], [First Name], [Last Name], [Birth Date]\n" +
+					 "from [CRONUS Sverige AB$Employee Relative]\n";
 			 break;
-			 case 5: stmt = "SELECT *\n" +
-					 "FROM [CRONUS Sverige AB$Employee Statistics Group]\n";
+			 case 5: stmt = "select *\n" +
+					 "from [CRONUS Sverige AB$Employee Statistics Group]\n";
 			 break;
-			 case 6: stmt = "SELECT *\n" +
-					 "FROM [CRONUS Sverige AB$Employment Contract]\n";
+			 case 6: stmt = "select *\n" +
+					 "from [CRONUS Sverige AB$Employment Contract]\n";
 			 break;
-			 case 7: stmt = "SELECT name, type FROM sys.key_constraints;";
+			 case 7: stmt = "select name, type from sys.key_constraints;";
 			 break;
-			 case 8: stmt = "SELECT CONSTRAINT_NAME, CONSTRAINT_TYPE\n" +
-					 "FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS";
+			 case 8: stmt = "select constraint_name, constraint_type\n" +
+			 "from information_schema.table_constraints";
 			 break;
-			 case 9: stmt = "SELECT TABLE_NAME\n" +
-					 "FROM INFORMATION_SCHEMA.TABLES\n";
+			 case 9: stmt = "select table name\n" +
+					 "from information_schema.tables\n";
 			 break;
-			 case 10: stmt = "SELECT COLUMN_NAME\n" +
-					 "FROM INFORMATION_SCHEMA.COLUMNS\n" +
-					 "WHERE TABLE_NAME = 'CRONUS Sverige AB$Employee'\n";
+			 case 10: stmt = "select column_name\n" +
+					 "from information_schema.columns\n" +
+					 "where table_name ='CRONUS Sverige AB$Employee'\n";
 			 break;
-			 case 11: stmt = "SELECT TABLE_NAME, COLUMN_NAME\n" +
-					 "FROM INFORMATION_SCHEMA.COLUMNS\n" +
-					 "WHERE TABLE_NAME LIKE '%Employee%'";
+			 case 11: stmt = "select table_name, column_name\n" +
+					 "from information_schema.columns\n" +
+					 "where table_name like '%Employee%'";
 			 break;
-			 case 12: stmt = "SELECT TOP 1\n" +
+			 case 12: stmt = "select top 1\n" +
 					 "    [TableName] = o.name, \n" +
 					 "    [RowCount] = i.rows\n" +
-					 "FROM \n" +
+					 "from \n" +
 					 "    sysobjects o, \n" +
 					 "    sysindexes i \n" +
-					 "WHERE \n" +
+					 "where \n" +
 					 "    o.xtype = 'U' \n" +
-					 "    AND \n" +
-					 "    i.id = OBJECT_ID(o.name)\n" +
-					 "ORDER BY i.rows DESC\n";
+					 "    and \n" +
+					 "    i.id = object_id(o.name)\n" +
+					 "order by i.rows desc\n";
 			 break;
-			 case 13: stmt = "SELECT [Relational Exch_ Rate Amount]\n" +
-					 "FROM [CRONUS Sverige AB$Currency Exchange Rate]\n" +
-					 "WHERE [Currency Code] = 'NOK'\n";
+			 case 13: stmt = "select [Relational Exch_ Rate Amount]\n" +
+					 "from [CRONUS Sverige AB$Currency Exchange Rate]\n" +
+					 "where [Currency Code] = 'NOK'\n";
 			 break;
-			 case 14: stmt = "SELECT [Currency Code]\n" +
-					 "FROM [CRONUS Sverige AB$Currency Exchange Rate]\n" +
-					 "WHERE ([Relational Exch_ Rate Amount] = (SELECT MAX([Relational Exch_ Rate Amount])\n" +
-					 "FROM [CRONUS Sverige AB$Currency Exchange Rate]))";
+			 case 14: stmt = "select [Currency Code]\n" +
+					 "from [CRONUS Sverige AB$Currency Exchange Rate]\n" +
+					 "where ([Relational Exch_ Rate Amount] = (select max([Relational Exch_ Rate Amount])\n" +
+					 "from [CRONUS Sverige AB$Currency Exchange Rate]))";
 			 break;
-			 case 15: stmt = "SELECT Address, City\n" +
-					 "FROM [CRONUS Sverige AB$Customer]\n" +
-					 "WHERE [Search Name] LIKE '%FOTOGRAFERNA%'\n";
+			 case 15: stmt = "select Address, City\n" +
+					 "from [CRONUS Sverige AB$Customer]\n" +
+					 "where [Search Name] like '%FOTOGRAFERNA%'\n";
 			 break;
-			 case 16: stmt = "SELECT DISTINCT emp.[Search Name]\n" +
-					 "FROM [CRONUS Sverige AB$Employee] emp\n" +
-					 "JOIN [CRONUS Sverige AB$Employee Absence] empAb\n" +
-					 "ON emp.No_ = empAb.[Employee No_]\n" +
-					 "WHERE empAb.[Cause of Absence Code] = 'SJUK'\n";
+			 case 16: stmt = "select distinct emp.[Search Name]\n" +
+					 "from [CRONUS Sverige AB$Employee] emp\n" +
+					 "join [CRONUS Sverige AB$Employee Absence] empAb\n" +
+					 "on emp.No_ = empAb.[Employee No_]\n" +
+					 "where empAb.[Cause of Absence Code] = 'SJUK'\n";
 			 break;
-			 case 17: stmt = "SELECT emp.[Search Name], empRel.[First Name], empRel.[Last Name], empRel.[Relative Code]\n" +
-					 "FROM [CRONUS Sverige AB$Employee Relative] empRel\n" +
-					 "JOIN [CRONUS Sverige AB$Employee] emp\n" +
-					 "ON empRel.[Employee No_] = emp.No_\n" +
-					 "ORDER BY emp.[Search Name]\n";
+			 case 17: stmt = "select emp.[Search Name], empRel.[First Name], empRel.[Last Name], empRel.[Relative Code]\n" +
+					 "from [CRONUS Sverige AB$Employee Relative] empRel\n" +
+					 "join [CRONUS Sverige AB$Employee] emp\n" +
+					 "on empRel.[Employee No_] = emp.No_\n" +
+					 "order by emp.[Search Name]\n";
 			 break;
-			 case 18: stmt = "SELECT cus.Name\n" +
-					 "FROM [CRONUS Sverige AB$Customer] cus\n" +
-					 "JOIN [CRONUS Sverige AB$Employee] emp\n" +
-					 "ON cus.[Salesperson Code] = emp.No_\n" +
-					 "WHERE emp.[First Name] = 'Andreas' AND emp.[Last Name] = 'Berglund'\n";
+			 case 18: stmt = "select cus.Name\n" +
+					 "from [CRONUS Sverige AB$Customer] cus\n" +
+					 "join [CRONUS Sverige AB$Employee] emp\n" +
+					 "on cus.[Salesperson Code] = emp.No_\n" +
+					 "where emp.[First Name] = 'Andreas' and emp.[Last Name] = 'Berglund'\n";
 			 break;
-			 case 19: stmt = "SELECT [Bank Account No_]\n" +
-					 "FROM [CRONUS Sverige AB$Customer Bank Account] \n" +
-					 "WHERE [CRONUS Sverige AB$Customer Bank Account].[Customer No_] = '10000'\n";
+			 case 19: stmt = "select [Bank Account No_]\n" +
+					 "from [CRONUS Sverige AB$Customer Bank Account] \n" +
+					 "where [CRONUS Sverige AB$Customer Bank Account].[Customer No_] = '10000'\n";
 			 break;
 		 }
 		 return stmt;
